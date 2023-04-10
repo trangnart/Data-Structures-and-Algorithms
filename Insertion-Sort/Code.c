69d89dd012a5767e77a2ea852d5c71f2d6f2e0fb
Jane Tran <ttran161@ucsc.edu>
2023-04-10 16:59:43 -0700

    fix insertion sort comment

--- Insertion-Sort/Code.c
+++ Insertion-Sort/Code.c
@@ -16,12 +16,12 @@ void append(List L, int x){
         L->cursor = L->back;    // the cursor is set to the back of the list
     }
     else{                       // the list is not empty
-        L->back->next = N;      // the 'back' node's 'next' pointer is point to new node N
+        L->back->next = N;      // the 'back' node's next pointer is point to new node N
         N->prev = L->back;      // N's 'prev' pointer is point to the 'back' node
         L->back = N;            // N becomes the new 'back' node of the list
-        N->next = NULL;         // N 'next' pointer is set to NULL
+        N->next = NULL;         // N next pointer is set to NULL
     }
-    L->length++;                // increment the 'length' of the list by 1
+    L->length++;                // increment the index of the list by 1
 }
 
 /* prepend()
@@ -37,15 +37,15 @@ void prepend(List L, int x){
 
     if (L->front == NULL){      // check if the list is empty
         L->front = L->back = N; // the new node N become both the 'front' & 'back' of the list
-        L->cursor = L->front;   // the 'cursor' is set to the 'front' of the list
+        L->cursor = L->front;   // the cursor is set to the 'front' of the list
     }
     else{                       // the list is not empty
-        L->front->prev = N;     // the 'previous' node of the current 'front' node is set to point to N
-        N->next = L->front;     // the 'next' node of N is set to point to the current 'front' node
+        L->front->prev = N;     // the previous node of the current 'front' node is set to point to N
+        N->next = L->front;     // the next node of N is set to point to the current 'front' node
         L->front = N;           // the 'front' of the list is updated to point to N
-        L->index++;             // increment the 'index' of the list by 1
+        L->index++;             // increment the index of the list by 1
     }
-    L->length++;                // increment the 'length' of the list by 1
+    L->length++;                // increment the index of the list by 1
 }
 
 /* moveFront()
@@ -56,8 +56,8 @@ void moveFront(List L){
         printf("List Error: calling moveFront() on an empty List\n");
         exit(EXIT_FAILURE);
     }
-    L->index = 0;         // set the 'index' of the list to 0
-    L->cursor = L->front; // set the 'cursor' of the list to the front
+    L->index = 0;         // set the index of the list to 0
+    L->cursor = L->front; // set the cursor of the list to the front
 }
 
 /* moveNext()
@@ -69,12 +69,12 @@ void moveNext(List L){
         printf("List Error: calling moveNext() on an empty List\n");
         exit(EXIT_FAILURE);
     }
-    if (L->cursor){                      // check if the 'cursor' is currently pointing to an element in the list
-        if (L->cursor == L->back)        // check if the current 'cursor' is pointing to the last element in the list
-            L->cursor = NULL;            // set 'cursor' to NULL
-        else                             // the current 'cursor' is NOT pointing to the last element in the list
-            L->cursor = L->cursor->next; // the 'cursor' is updated to point to the next following node
-        L->index++;                      // increment the 'index' of the list by 1
+    if (L->cursor){                      // check if the cursor is currently pointing to an element in the list
+        if (L->cursor == L->back)        // check if the current cursor is pointing to the last element in the list
+            L->cursor = NULL;            // set cursor to NULL
+        else                             // the current cursor is NOT pointing to the last element in the list
+            L->cursor = L->cursor->next; // the cursor is updated to point to the next following node
+        L->index++;                      // increment the index of the list by 1
     }
 }
 
@@ -88,16 +88,16 @@ void insertBefore(List L, int x){
         printf("List Error: calling insertBefore() on an empty List\n");
         exit(EXIT_FAILURE);
     }
-    if (L->cursor == L->front)     // check if the 'cursor' is pointing to the first element of the list
+    if (L->cursor == L->front)     // check if the cursor is pointing to the first element of the list
         prepend(L, x);             // call 'prepend()' to insert the new node at the beginning of the list
-    else{                          // the 'cursor' is not pointing to the first element of the list
+    else{                          // the cursor is not pointing to the first element of the list
         Node N = newNode(x);       // create a new node with value 'x'
-        N->prev = L->cursor->prev; // set the 'previous' pointer of a new node N to point to the node that precedes the current 'cursor' position
-        N->next = L->cursor;       // set the 'next' pointer of a new node N to point to the current 'cursor' position
-        L->cursor->prev->next = N; // set the 'next' pointer of the node that precedes the current 'cursor' position to point to a new node N
-        L->cursor->prev = N;       // set the 'previous' pointer of the current 'cursor' to point to a new node N
-        L->index++;                // increment the 'index' of the list by 1
-        L->length++;               // increment the 'length' of the list by 1
+        N->prev = L->cursor->prev; // set the previous pointer of a new node N to point to the node that precedes the current cursor position
+        N->next = L->cursor;       // set the next pointer of a new node N to point to the current cursor position
+        L->cursor->prev->next = N; // set the next pointer of the node that precedes the current cursor position to point to a new node N
+        L->cursor->prev = N;       // set the previous pointer of the current cursor to point to a new node N
+        L->index++;                // increment the index of the list by 1
+        L->length++;               // increment the index of the list by 1
    }
 }
 
